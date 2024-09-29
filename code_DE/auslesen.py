d6f8e83c05986501cc7a52618028a427d44cce7d
Veicm <connormannes@gmx.de>
2024-09-29 15:56:15 +0000

    Update auslesen.py

--- code_DE/auslesen.py
+++ code_DE/auslesen.py
@@ -1,17 +1,18 @@
+#Dieser Code ist geignet um die signale eines joysticks auszuleasen um sie in einem anderen script zu nutzen.
 from machine import Pin, ADC
 import utime
 
-xAxis=ADC(Pin(27))
-yAxis=ADC(Pin(26))
-button=Pin(1, Pin.IN, Pin.PULL_UP)
+x_achse=ADC(Pin(27)) #Der x-achse des Joysticks wird Pin 27 zugewiesen
+y_achse=ADC(Pin(26)) #Der y-achse des Joysticks wird Pin 26 zugewiesen
+knopf=Pin(1, Pin.IN, Pin.PULL_UP) #Wenn man den stick runterdrückt geht das signal an Pin 1
 
 while True:
-    xValue = xAxis.read_u16()
-    yValue = yAxis.read_u16()
-    buttonValue = button.value()
+    x_wert = x_achse.read_u16()
+    y_wert = y_achse.read_u16()
+    knopf_wert = knopf.value()
     
     
-    print("X Value:", xValue, "Y Value:", yValue, "Button Value:", buttonValue)
+    print("X Wert:", x_wert, "Y Wert:", y_wert, "Knopf Wert:", knopf_wert)
     
     
     utime.sleep(1)
